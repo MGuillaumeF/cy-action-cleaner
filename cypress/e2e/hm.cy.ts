14382e663b6648cc113032b36cfee9698c186fd3
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-10-14 08:15:05 +0200

    Update hm.cy.ts

--- cypress/e2e/hm.cy.ts
+++ cypress/e2e/hm.cy.ts
@@ -2,7 +2,12 @@ import { expect } from "chai";
 
 describe("Page Access", () => {
   it("Visite page", () => {
-    cy.visit("/hangman-game/actions");
+    cy.visit("/hangman-game/actions", {headers : {
+
+"Accept": "application/vnd.github.v3+json",
+"Authorization": ˋBearer ${process.env.GITHUB_TOKEN}`
+
+    }});
     cy.get("body");
   });
 });
